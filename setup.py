51f3ecfedb8c2cbd7de3d3e1eed2686ddca06959
Jacob Kravits <49840473+kravitsjacob@users.noreply.github.com>
2022-12-01 16:25:59 -0700

    Setup dockerfile

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="submission",
+    version="0.0.0b0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.9",
+)
